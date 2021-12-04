d8c0ba701767aaf284c30acaa8c10c292cbcf29f
Igor Jankowski <jankowskiigor5@gmail.comgit congig --global -lgit config --global user.email jankowskiigor5@gmail.com>
2021-12-04 20:48:01 +0100

    Updates

---
+++ precache-manifest.8494905e2c0613552e87b7220c2f7253.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb634fbaf153bfbb4f76e9a0645654a",
+    "url": "/quotes/index.html"
+  },
+  {
+    "revision": "f2482bc01f335533036f",
+    "url": "/quotes/static/css/main.64877bdb.chunk.css"
+  },
+  {
+    "revision": "a94775cab844c13bb645",
+    "url": "/quotes/static/js/2.2a619a19.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/quotes/static/js/2.2a619a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1715763f3ba90956900a",
+    "url": "/quotes/static/js/3.13972ffc.chunk.js"
+  },
+  {
+    "revision": "f2482bc01f335533036f",
+    "url": "/quotes/static/js/main.d1a8e502.chunk.js"
+  },
+  {
+    "revision": "6c059dcde9dbe59d9e1c",
+    "url": "/quotes/static/js/runtime-main.8a2341ae.js"
+  }
+]);
